4f770ea462ecf894c7c3feeded5d6fcffa19d592
Ana Pujana <anapujanajimenez@gmail.com>
2025-05-02 13:42:05 +0200

    Documentation edits made through Mintlify web editor

--- ar.js
+++ ar.js
@@ -1,3 +1,14 @@
+if (document.readyState !== 'loading') {
+  // Document is already ready
+  const html = document.documentElement;
+  const lang = html.lang.toLowerCase();
+
+  if (lang === "ar") {
+    html.setAttribute("dir", "rtl");
+  } else {
+    html.setAttribute("dir", "ltr");
+  }
+} else {
   document.addEventListener("DOMContentLoaded", function () {
     const html = document.documentElement;
     const lang = html.lang.toLowerCase();
@@ -8,3 +19,4 @@
       html.setAttribute("dir", "ltr");
     }
   });
+}
